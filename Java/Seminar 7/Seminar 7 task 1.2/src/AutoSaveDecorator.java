0bfdfef28628b48e233d7036ca1fedcda3b4e4d8
root <filiperamos212@gmail.com>
2025-11-19 14:39:11 +0000

    Task 1.2 complete for seminar 7

--- Java/Seminar 7/Seminar 7 task 1.2/src/AutoSaveDecorator.java
+++ Java/Seminar 7/Seminar 7 task 1.2/src/AutoSaveDecorator.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class AutoSaveDecorator extends DocumentDecorator {
 
     public AutoSaveDecorator(Document document) {
@@ -5,11 +9,14 @@ public class AutoSaveDecorator extends DocumentDecorator {
     }
 
     public void saveDocument() {
-        System.out.println("Saving document");
-        // TODO: add saving logic
-        //       - Should save to file after every write
-        //       - Display save confirmation
-
+        System.out.println("Saving document...");
+        try {
+            Files.writeString(Path.of("document.txt"), document.read());
+            System.out.println("Document saved successfully.");
+        }
+        catch (IOException e) {
+            System.err.println("Failed to save document: " + e.getMessage());
+        }
     }
 
     @Override
